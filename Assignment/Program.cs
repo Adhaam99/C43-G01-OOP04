9bdd5ef2ed9b1e75c80d3913d7dd362beec2b853
adham yehia <adhamyehia194@gmail.com>
2025-01-16 18:17:54 +0200

    Q2

--- Assignment/Program.cs
+++ Assignment/Program.cs
@@ -1,10 +1,45 @@
-﻿namespace Assignment
+﻿
+
+namespace Assignment
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Part01
+
+            #region Q1 -  Write a class named Calculator that contains a method named Add. Overload the Add method to:
+
+
+            //Calculator calculator = new Calculator();
+
+            //Console.WriteLine(calculator.Add(10, 20));
+
+            //Console.WriteLine(calculator.Add(10, 20, 20));
+
+            //Console.WriteLine(calculator.Add(10.5, 20.1));
+
+            #endregion
+
+            #region Q2 - Create a class named Rectangle with the following constructors:
+
+            //Rectangle rectangle = new Rectangle();
+
+            //Console.WriteLine(rectangle.ToString());
+
+            //Rectangle recatngle = new Rectangle(10 , 20);
+
+            //Console.WriteLine(recatngle);
+
+            //Rectangle recatngle = new Rectangle(10);
+
+            //Console.WriteLine(recatngle);
+
+            #endregion
+
+
+
+            #endregion
         }
     }
 }
