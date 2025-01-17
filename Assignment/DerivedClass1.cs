20b6efca9e7dcb4b77b535471791ba5bcc107f47
adham yehia <adhamyehia194@gmail.com>
2025-01-17 23:08:01 +0200

    Q5

---
+++ Assignment/DerivedClass1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment
+{
+    class DerivedClass1: BaseClass
+    {
+        #region Methods
+
+        public override void DisplayMessage()
+        {
+            Console.WriteLine("This is DerivedClass1");
+        }
+
+        #endregion
+    }
+}
--- Assignment/Program.cs
+++ Assignment/Program.cs
@@ -8,73 +8,100 @@ namespace Assignment
         {
             #region Part01
 
-            #region Q1 -  Write a class named Calculator that contains a method named Add. Overload the Add method to:
+#region Q1 -  Write a class named Calculator that contains a method named Add. Overload the Add method to:
 
 
-            //Calculator calculator = new Calculator();
+//Calculator calculator = new Calculator();
 
-            //Console.WriteLine(calculator.Add(10, 20));
+//Console.WriteLine(calculator.Add(10, 20));
 
-            //Console.WriteLine(calculator.Add(10, 20, 20));
+//Console.WriteLine(calculator.Add(10, 20, 20));
 
-            //Console.WriteLine(calculator.Add(10.5, 20.1));
+//Console.WriteLine(calculator.Add(10.5, 20.1));
 
-            #endregion
+#endregion
 
-            #region Q2 - Create a class named Rectangle with the following constructors:
+#region Q2 - Create a class named Rectangle with the following constructors:
 
-            //Rectangle rectangle = new Rectangle();
+//Rectangle rectangle = new Rectangle();
 
-            //Console.WriteLine(rectangle.ToString());
+//Console.WriteLine(rectangle.ToString());
 
-            //Rectangle recatngle = new Rectangle(10 , 20);
+//Rectangle recatngle = new Rectangle(10 , 20);
 
-            //Console.WriteLine(recatngle);
+//Console.WriteLine(recatngle);
 
-            //Rectangle recatngle = new Rectangle(10);
+//Rectangle recatngle = new Rectangle(10);
 
-            //Console.WriteLine(recatngle);
+//Console.WriteLine(recatngle);
 
-            #endregion
+#endregion
 
-            #region Q3 - Define a class Complex Number that represents a complex number with real and imaginary parts.
+#region Q3 - Define a class Complex Number that represents a complex number with real and imaginary parts.
 
-            //ComplexNumber C1 = new ComplexNumber() { Real = 7 , Img = 3};
+//ComplexNumber C1 = new ComplexNumber() { Real = 7 , Img = 3};
 
-            //ComplexNumber C2 = new ComplexNumber() { Real = 10, Img = 6 };
+//ComplexNumber C2 = new ComplexNumber() { Real = 10, Img = 6 };
 
-            ////ComplexNumber C3 = C1 + C2;
+////ComplexNumber C3 = C1 + C2;
 
-            ////Console.WriteLine(C3);
+////Console.WriteLine(C3);
 
-            //ComplexNumber C3 = C1 - C2;
+//ComplexNumber C3 = C1 - C2;
 
-            //Console.WriteLine(C3);
+//Console.WriteLine(C3);
 
-            #endregion
+#endregion
 
-            #region Q4 
+#region Q4 
 
-            #region a) Create a base class named Employee with method That Work as it prints "Employee is working".
+#region a) Create a base class named Employee with method That Work as it prints "Employee is working".
 
-            //Employee employee = new Employee();
+//Employee employee = new Employee();
 
-            //employee.Work();
+//employee.Work();
 
-            #endregion
+#endregion
 
-            #region b) Create a derived class named Manager that overrides the Work method to print "Manager is managing".
+#region b) Create a derived class named Manager that overrides the Work method to print "Manager is managing".
 
-            //Manager manager = new Manager();
+//Manager manager = new Manager();
 
-            //manager.Work();
+//manager.Work();
 
-            #endregion
+#endregion
 
-            #endregion
+#endregion
+
+#region Q5
+
+//BaseClass baseClass = new BaseClass();
+
+//baseClass.DisplayMessage();
+
+//DerivedClass1 D1 = new DerivedClass1();
+
+//D1.DisplayMessage();
+
+//DerivedClass2 D2 = new DerivedClass2();
+
+//D2.DisplayMessage();
+
+/*
+ * The override keyword is used to extend or modify the abstract or virtual implementation of an inherited method, property, indexer, or event.
+ * 
+ * When a method in a derived class is marked with override, it replaces the method with the same signature in the base class. The base class method must be marked with virtual, abstract, or override.
+ * 
+ * 
+ * The new keyword is used to hide a member of the base class. This means you are creating a new member in the derived class that has the same name as one in the base class.
+ * 
+ * When a method in a derived class is marked with new, it hides the method with the same signature in the base class. This does not replace the base class method but creates a new method in the derived class.
 
 
-            #endregion
-        }
-    }
+#endregion
+
+
+#endregion
+}
+}
 }
